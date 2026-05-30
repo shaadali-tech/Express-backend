25be293cf378c408c1a792cceac83778d676714f
shaadali-tech <215ics043@gbu.ac.in>
2026-05-30 09:42:33 +0530

    added files

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.write("Hello World");
-  res.end();
+  let filePath = "./pages";
+  console.log(req.url);
+  console.log(req.method);
+  if (req.url === "/") {
+    filePath = path.join(__dirname, "pages", "home.html");
+  } else if (req.url === "/about") {
+    filePath = path.join(__dirname, "pages", "about.html");
+  } else if (req.url === "/contact") {
+    filePath = path.join(__dirname, "pages", "contact.html");
+  } else {
+    filePath = path.join(__dirname, "pages", "404.html");
+  }
+
+  if (filePath) {
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 - Page not found");
+  }
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
